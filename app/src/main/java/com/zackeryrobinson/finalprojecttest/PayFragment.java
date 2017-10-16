9eec565ea8e64fc2e8a2e3837978878627e60a09
Zack Robinson <zwrprofessional@gmail.com>
2017-10-15 22:47:18 -0400

    Updated the date_card

--- app/src/main/java/com/zackeryrobinson/finalprojecttest/PayFragment.java
+++ app/src/main/java/com/zackeryrobinson/finalprojecttest/PayFragment.java
@@ -27,7 +27,7 @@ public class PayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        return inflater.inflate(R.layout.fragment_pay_stub, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
